ee937cdde106b079a5ab89949228401a43fa7b49
Pawel Dybcio <pawel.dybcio@testarmy.com>
2020-08-05 22:06:23 +0200

    reformat code

--- src/com/company/Webinar7/Student.java
+++ src/com/company/Webinar7/Student.java
@@ -1,17 +1,20 @@
 package com.company.Webinar7;
 
-public class Student extends Person{
+public class Student extends Person {
 
     private String fieldOfStudy;
     private int semester;
+
     public Student(String firstName, String lastName, int age, String fieldOfStudy, int semester) {
         super(firstName, lastName, age);
         this.fieldOfStudy = fieldOfStudy;
         this.semester = semester;
     }
+
     private int calculateYearOfSemester() {
-        return  (int) Math.ceil((double) semester / 2);
+        return (int) Math.ceil((double) semester / 2);
     }
+
     @Override
     public void displayInformationAboutMe() {
         super.displayInformationAboutMe();
